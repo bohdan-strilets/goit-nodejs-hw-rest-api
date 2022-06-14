127660f5012fa00b50061604475cd75b02e5ea6a
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-06-14 21:21:32 +0200

    added login and signup

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,7 @@
-const getAll = require("./contacts/getAll");
-const getById = require("./contacts/getById");
-const add = require("./contacts/add");
-const deleteById = require("./contacts/deleteById");
-const updateById = require("./contacts/updateById");
-const updateFavorite = require("./contacts/updateFavorite");
+const contacts = require("./contacts");
+const auth = require("./auth");
 
 module.exports = {
-  getAll,
-  getById,
-  add,
-  deleteById,
-  updateById,
-  updateFavorite,
+  contacts,
+  auth,
 };
---
+++ routes/api/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const { auth: ctrl } = require("../../controllers");
+const { joiSchema } = require("../../models/user");
+const { validation, ctrlWrapper } = require("../../middlewares");
+const router = express.Router();
+
+router.post("/signup", validation(joiSchema), ctrlWrapper(ctrl.signup));
+router.post("/login", validation(joiSchema), ctrlWrapper(ctrl.login));
+
+module.exports = router;
