617be33bd58314546c199ce6db9bcef52c475167
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-11-07 18:40:59 +0100

    change origin for cors

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "https://bohdan-strilets.github.io",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.static("./public"));
 
